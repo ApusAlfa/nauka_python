15dae94178febab98ee1abdb511b59cbe2801f89
ApusAlfa <ApusAlfa@user.noreply.github.com>
2020-12-05 14:01:16 +0100

    Moj commit 5

---
+++ exercise_8.py
@@ -0,0 +1,14 @@
+print('zadanie8!')
+
+menu = ['danie dnia', 'polecane']
+
+print(menu[0])
+print(menu[1])
+
+for d in menu:
+    print(d)
+
+for y in range( len(menu) ):
+    print("y: " + str(y) + ": " + menu[y])
+
+print(menu[3])
