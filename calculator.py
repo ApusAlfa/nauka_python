b7f01398ab19cfe9d5bc81a709a9ea288cefe511
Marta Herezo <marta.herezo@gmail.com>
2020-12-06 12:20:38 +0000

    added subtraction

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
 def calculate(a, b, operacja):
   if operacja == "+":
     return a + b
+  if operacja == "-":
+    return a - b
+  return 0
--- test_calculator.py
+++ test_calculator.py
@@ -6,3 +6,6 @@ class TestCalculator(unittest.TestCase):
   def test_dodawanie(self):
     r = calculate(1, 2, '+')
     self.assertEqual(r, 3)
+  def test_odejmowanie(self):
+    r = calculate(10, 120, "-")
+    self.assertEqual(r, -110)
