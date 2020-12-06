128f5e94dede72d1945119b4c554d0efa3cb1e87
Marta Herezo <marta.herezo@gmail.com>
2020-12-06 14:49:45 +0000

    added exercise #8

--- new_calculator.py
+++ new_calculator.py
@@ -1,4 +1,11 @@
 def str_calculate(a, b, operacja):
   if operacja == 'concat':
     return a + b
+  if operacja == 'awb':
+    return a in b
+  if operacja == 'last_sign_in_b_shbe_a':
+    return a[0] in b[-1]
+   #print(b[-1])
+  if operacja == 'last_sign_in_a_shbe_b':
+    return a[-1] in b[0]
   return 0
--- new_test_calculator.py
+++ new_test_calculator.py
@@ -7,3 +7,28 @@ class TestCalculator(unittest.TestCase):
     r = str_calculate(1, 2, 'concat')
     self.assertEqual(r, 3)
     print('Obecna wartosc dodawania: ' + str(r))
+
+class TestStringCalculator(unittest.TestCase):
+  def test_concat(self):
+    r = str_calculate("a", "b", 'concat')
+    self.assertEqual(r, 'ab')
+    print('Obecna wartosc ab: ' + str(r))
+
+  def test_awb1(self):
+    r = str_calculate("a", "b", "awb")
+    self.assertEqual(r, False)
+
+  def test_awb2(self):
+    r = str_calculate("a", "bab", "awb")
+    self.assertEqual(r, True)
+
+  def test_last1(self):
+    r = str_calculate("a", "ba", "last_sign_in_b_shbe_a")
+    self.assertEqual(r, True)
+    print(r)
+  
+  def test_last2(self):
+    r = str_calculate("aba", "b", "last_sign_in_a_shbe_b")
+    self.assertEqual(r, True)
+    print(r)
+
