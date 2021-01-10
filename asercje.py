e23e60d51bc1e351c676da3df56333101476c1d1
Marta Herezo <marta.herezo@gmail.com>
2021-01-10 11:33:13 +0000

    assercja prawidłowa

--- asercje.py
+++ asercje.py
@@ -1,36 +1,66 @@
+# import unittest
+# #SZABLON
+# # #Asercje
+# # #"Czysty" Python
+# # assert 3 > 4
+
+# class MojPrzypadekTestowy(unittest.TestCase):
+#   #Przygotowanie do testu
+#   #(Analogia: Warunki wstępne)
+#   def setUp(self):
+#     #pass
+#     print("setUp")
+
+#   #Sprzątanie po teście
+#   def tearDown(self):
+#     #pass
+#     print(tearDown)
+
+#   #Metoda testowa (musi zaczynać się od słowa "test")
+#   def testPierwszy(self):
+#     #pass
+#     #Czysty Python
+#     assert 1 == 1
+#     # To samo przy wykorzystaniu unittesta
+#     self.assertEqual(1,1)
+#     # Przykład
+#     #self.assertEqual(komunikat, "Zły email") porównuje komunikat z tym na stronie.
+
+# ###################
+
+#   def testDrugi(self):
+#     pass
+
+
+# if __name__ == "__main__":
+#   unittest.main(verbosity=2)
+
+
+##############
 import unittest
-#SZABLON
-# #Asercje
-# #"Czysty" Python
-# assert 3 > 4
 
 class MojPrzypadekTestowy(unittest.TestCase):
-  #Przygotowanie do testu
-  #(Analogia: Warunki wstępne)
-  def setUp(self):
-    #pass
-    print("setUp")
+    # Przygotowanie do testu
+    # (Analogia: Warunki wstępne)
+    def setUp(self):
+        print("setUp")
 
-  #Sprzątanie po teście
-  def tearDown(self):
-    #pass
-    print(tearDown)
+    # Sprzątanie po teście
+    def tearDown(self):
+        print("tearDown")
 
-  #Metoda testowa (musi zaczynać się od słowa "test")
-  def testPierwszy(self):
-    #pass
-    #Czysty Python
-    assert 1 == 1
-    # To samo przy wykorzystaniu unittesta
-    self.assertEqual(1,1)
-    # Przykład
-    #self.assertEqual(komunikat, "Zły email") porównuje komunikat z tym na stronie.
+    # Metoda testowa (musi zaczynać się od słowa "test")
+    def testPierwszy(self):
+        print("test")
+        # Czysty Python
+        assert 1 == 1
+        # To samo przy wykorzystaniu unittesta
+        self.assertEqual(1, 1)
 
-###################
-
-  def testDrugi(self):
-    pass
+    def testDrugi(self):
+        print("test")
+        pass
 
 
 if __name__ == "__main__":
-  unittest.main(verbosity=2)
+    unittest.main(verbosity=2)
